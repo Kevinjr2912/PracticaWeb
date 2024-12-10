703d0b9a120b55241989882c8c3cf2db7b32d1a5
Kevin <moralesperezkevin7@gmail.com>
2024-12-10 09:15:25 -0600

    agregando el modal-personaje

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,57 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { EpisodioService } from './episodios/servicios/episodio.service';
-import { Episodios } from './episodios/modelos/episodios';
-import { InformaciónEpisodio } from './episodios/modelos/información-episodio';
-import { PersonajeService } from './personajes/servicios/personaje.service';
-import { InformacionPersonaje } from './personajes/modelos/informacion-personaje';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent implements OnInit{
-  episodiosObtenidos: Episodios = {
-    results :[{
-      id: 0,
-      name: '',
-      air_date: '',
-      episode: '',
-      characters: [],
-      url: '',
-      created: new Date()
-    }]
-  }
-
-  personajes: InformacionPersonaje [] = [];
-
-  constructor(private servicioEpisodio: EpisodioService, private servicioPersonaje: PersonajeService ){}
-
-  goBack(): void {
-    window.history.back()
-  }
-
-  ngOnInit(): void {
-  
-  }
-
-  /* asociarInformacionPersonaje(): void {
-    this.episodiosObtenidos.results.forEach(episodio => {
-      let episodioInformación: InformaciónEpisodio = {
-        id: episodio.id,
-        name: episodio.name,
-        air_date: episodio.air_date,
-        episode: episodio.episode,
-        characters: episodio.characters,
-        url: episodio.url
-      }
-
-      this.episodios.push(episodioInformación);
-    }); 
-
-  }*/
-  
-  seeEpisodes(): void {}
-
-  seeFavorites(): void {}
-}
+export class AppComponent{}
--- src/app/personajes/personajes.module.ts
+++ src/app/personajes/personajes.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardCharacterComponent } from './card-character/card-character.component';
 import { ListCharacterComponent } from './list-character/list-character.component';
+import { ModalPersonajeComponent } from './modal-personaje/modal-personaje.component';
 
 
 @NgModule({
   declarations: [
     CardCharacterComponent,
-    ListCharacterComponent
+    ListCharacterComponent,
+    ModalPersonajeComponent
   ],
   imports: [
     CommonModule
